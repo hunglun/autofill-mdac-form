aabec865655117dcd183b75320d4b3c93fc0c044
O Hung Lun <hunglun.o@gmail.com>
2023-10-18 16:35:02 +0800

    turned javascript to chrome extension

---
+++ chrome-autofill-mdac-extension/scripts/content.js
@@ -0,0 +1,15 @@
+document.getElementById("nationality").selectedIndex = "212";
+document.getElementById("passNo").value = "M1230123K";
+document.getElementById("name").value = "Charlie";
+document.getElementById("sex").value = "1";
+document.getElementById("dob").value = "01/08/1991";
+document.getElementById("email").value = "aa@gmail.com";
+document.getElementById("confirmEmail").value = "aa@gmail.com";
+document.getElementById("region").value = "+65";
+document.getElementById("mobile").value = "12341234";
+document.getElementById("confirmRegion").value = "+65";
+document.getElementById("confirmMobile").value = "12341234";
+document.getElementById("trvlMode").value = "2";
+document.getElementById("passExpDte").value = "01/11/2030";
+document.getElementById("arrDt").value = "18/10/2023";
+document.getElementById("depDt").value = "17/11/2023";
